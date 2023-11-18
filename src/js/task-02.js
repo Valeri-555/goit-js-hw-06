6d1fd4e686648a036695db8757aa4ee865a2da8f
Valeri-555 <106805806+Valeri-555@users.noreply.github.com>
2023-11-19 00:55:47 +0200

    create task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,25 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsId = document.querySelector('#ingredients');
+console.dir(ingredientsId);
+
+const items = ingredients.map((ingredient) => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  return item;
+});
+
+ingredientsId.append(...items);
+
+// const ingredientsId = document.getElementById('ingredients');
+// console.dir(ingredientsId);
+
+
+// const list = document.createElement('li');
+// const text = document.createElement('p');
+// console.dir(list);
+// console.dir(text);
+
