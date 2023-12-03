fa16bdeb9547121318a3ceb11289b53aac336f09
Valeri-555 <106805806+Valeri-555@users.noreply.github.com>
2023-12-03 20:06:41 +0200

    created script  to task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
 
+const form = document.querySelector('.login-form');
+console.dir(form);
+
+
+form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+    elements: { username, password }
+  } = event.currentTarget;
+    console.log(username.value, password.value);
+    
+     if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+     }
+    const userDetails = { email: email.value, Password: password.value }
+
+  console.log(userDetails);
+  event.currentTarget.reset();
+};
+
