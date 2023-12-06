f727975b0913383518421a17f3cbb03df6574fb3
Valeri-555 <106805806+Valeri-555@users.noreply.github.com>
2023-12-07 00:03:49 +0200

    update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,17 +2,15 @@ const input = document.querySelector ("#validation-input");
 
 
 input.addEventListener ("blur", () => {
-    const inputlength = input.value.length;
-    const datalength = input.getAttribute("data-length");
+    const inputLength = input.value.length;
+    const dataLength = input.getAttribute("data-length");
     const dataValue = parseInt(input.getAttribute("data-length"));
     
-    if (inputLength === datavalue) {
-        input.classlist.add("valid");
+    if (inputLength === dataValue) {
+        input.classList.add("valid");
         input.classList.remove("invalid");
     } else {
-        input.classlist.add("invalid");
+        input.classList.add("invalid");
         input.classList.remove("valid")
     }   
-console.log(inputLength);
-console.log(dataValue);
 });
